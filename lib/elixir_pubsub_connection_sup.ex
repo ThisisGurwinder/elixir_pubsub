bdba806ffaa336cc7a42b65f1666e3bd992df4e4
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-28 14:42:50 +0530

    werwerwer

--- lib/elixir_pubsub_connection_sup.ex
+++ lib/elixir_pubsub_connection_sup.ex
@@ -1,11 +1,12 @@
 defmodule ElixirPubsubConnection.Supervisor do
     defstruct parent: nil
     def start_link(_) do
-        spawn_link(__MODULE__, :init, [self])
+        spawn_link(__MODULE__, :init, [self()])
     end 
 
     def start_connection(_From, _Type, _Token) do
         send __MODULE__, {__MODULE__, :start_connection, From, Type, Token}
+        receive do Ret -> Ret end
     end
 
     def init(Parent) do
@@ -20,14 +21,14 @@ defmodule ElixirPubsubConnection.Supervisor do
         receive do
             {__MODULE__, :start_connection, From, Type, Token} ->
                 IO.puts "Started"
-        #         case ElixirPubsubConnection.start_link(From, Type) of
-        #             {:ok, pid} ->
-        #                 # send From, {:ok, pid}
-        #                 IO.puts "Started Elixir Pubsub Connection Supervisor"
-        #                 loop(State, CurConns+1)
-        #             _ ->
-        #                 loop(State, CurConns)
-        #         end;
+                case ElixirPubsubConnection.start_link(From, Type) of
+                    {:ok, pid} ->
+                        send From, {:ok, pid}
+                        IO.puts "Started Elixir Pubsub Connection Supervisor"
+                        loop(State, CurConns+1)
+                    _ ->
+                        loop(State, CurConns)
+                end;
             Msg ->
                 # send From, self()
                 IO.puts "Unknown Message Recieved #{Msg}"
