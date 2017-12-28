aa5596ff845a071b384df56caec82480ef8ddd7c
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-28 19:53:12 +0530

    Werwewer

--- lib/elixir_pubsub_connection.ex
+++ lib/elixir_pubsub_connection.ex
@@ -2,11 +2,11 @@ defmodule ElixirPubsubConnection do
    use GenServer
 
    def start_link(From, Type) do
-        GenServer.start_link(__MODULE__, [From, Type])
+        GenServer.start_link(__MODULE__, :init, [From, Type])
    end 
 
    def start(From, Type) do
-        GenServer.start(__MODULE__, [From, Type])
+        GenServer.start(__MODULE__, :init, [From, Type])
    end
 
    def init([from, :permanent]) do
