ee92e5e722928672b7c3def1e8712400bcb0a2f5
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-28 18:09:58 +0530

    Weewr

--- lib/elixir_pubsub_connection_sup.ex
+++ lib/elixir_pubsub_connection_sup.ex
@@ -8,7 +8,7 @@ defmodule ElixirPubsubConnection.Supervisor do
 
     def start_connection(from, type, token) do
         IO.puts "GOing to start connection"
-        send __MODULE__, {__MODULE__, :start_connection, from, type, token}
+        send __MODULE__, {:start_connection, from, type, token}
         receive do Ret -> Ret end
     end
 
@@ -20,23 +20,22 @@ defmodule ElixirPubsubConnection.Supervisor do
     end
 
     def loop(%ElixirPubsubConnection.Supervisor{parent: parent} = state, curConns) do
-        IO.puts "Done"
         receive do ->
-            {__MODULE__, :start_connection, from, type, token} ->
-                IO.puts "Got the Start Connection #{inspect(from)} #{inspect(type)}"
-                case ElixirPubsubConnection.start_link(from, type) do
-                    {:ok, pid} ->
-                        send from, {:ok, pid}
-                        IO.puts "Started Elixir Pubsub Connection Supervisor"
-                        loop(state, curConns+1)
-                    _ ->
-                        IO.puts "Got the empty response"
-                        send from, {:ok, self()}
-                        loop(state, curConns)
-                end;
+            # {:start_connection, from, type, token} ->
+            #     IO.puts "Got the Start Connection #{inspect(from)} #{inspect(type)}"
+            #     case ElixirPubsubConnection.start_link(from, type) do
+            #         {:ok, pid} ->
+            #             send from, {:ok, pid}
+            #             IO.puts "Started Elixir Pubsub Connection Supervisor"
+            #             loop(state, curConns+1)
+            #         _ ->
+            #             IO.puts "Got the empty response"
+            #             send from, {:ok, self()}
+            #             loop(state, curConns)
+            #     end;
             msg ->
                 # send From, self()
-                IO.puts "Unknown Message Recieved #{msg}"
+                IO.puts "Unknown Message Recieved #{inspect(msg)}"
                 loop(state, curConns)
         end
     end
