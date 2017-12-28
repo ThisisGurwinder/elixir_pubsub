06b64e8482597bf7e5e869ae84a4e0d1eded98fb
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-28 11:59:28 +0530

    EWrre

--- lib/elixir_pubsub_socket_handler.ex
+++ lib/elixir_pubsub_socket_handler.ex
@@ -6,27 +6,32 @@ defmodule ElixirPubsubSocketHandler do
         {:cowboy_websocket, req, %{:connection => nil}}
     end
 
-    def terminate(_reason, _req, _state) 
-    :do :ok
-    
+    def terminate(_reason, _req, _state)  do 
+        :ok
+    end
+
     def websocket_handle({:text, _content}, req, state = %{:connection => nil}) do
         newState = %{:connection => "EXIST"}
         {:reply, {:text, "NIL"}, req, newState}
     end
 
-    def websocket_handle({:text, _content}, req, state = %{:connection => _Something }) 
-    :do {:reply, {:text, "EXIST"}, req, state}
+    def websocket_handle({:text, _content}, req, state = %{:connection => _Something }) do
+        {:reply, {:text, "EXIST"}, req, state}
+    end
 
-    def websocket_handle(_frame, _req, state)
-    :do {:ok, state}
+    def websocket_handle(_frame, _req, state) do 
+        {:ok, state}
+    end
 
-    def websocket_info(_info, _req, state) 
-    :do {:ok, state}
+    def websocket_info(_info, _req, state)  do
+        {:ok, state}
+    end
 
     def create_connection(permanent) do
         {ok, CPid} = 
     end
 
-    def init_long_lived 
-    :do create_connection(permanent)
+    def init_long_lived do
+        create_connection(permanent)
+    end
 end
