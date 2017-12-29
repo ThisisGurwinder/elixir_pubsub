1db8b0af5d2f1bf5630b1400877e940504053f84
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-29 23:28:18 +0530

    werwer

--- lib/elixir_pubsub_socket_handler.ex
+++ lib/elixir_pubsub_socket_handler.ex
@@ -2,44 +2,39 @@ defmodule ElixirPubsubSocketHandler do
     @behaviour :cowboy_websocket
 
     def init(req, state) do
-        IO.puts "About to initialize Socket"
-        # {:ok, cpid} = init_long_lived()
+        {:ok, cpid} = init_long_lived()
         :erlang.start_timer(1000, self, [])
-        # _connection = %{ :connection => cpid }
-        {:cowboy_websocket, req, state}
+        connection = %{ :connection => cpid }
+        {:cowboy_websocket, req, connection}
     end
 
     def terminate(_reason, _req, _state) do
-    :ok
-  end
-  
+        :ok
+    end
+
     def websocket_handle({:text, content}, req, state) do
-    { :ok, %{ "message" => message} } = JSEX.decode(content)
+        {:reply, {:text, "ping"}, req, state}
+    end
+    def websocket_handle(_frame, _req, state) do
+        {:ok, state}
+    end
+    
+    def websocket_info({_timeout, _ref, _msg}, req, state) do
+        time = time_as_string()
+        { :ok, message } = JSEX.encode(%{ time: time})
+        :erlang.start_timer(1000, self, [])
+        { :reply, {:text, message}, req, state}
+    end
+    def websocket_info(_info, _req, state) do
+        {:ok, state}
+    end
 
-    rev = String.reverse(message)
-    { :ok, reply } = JSEX.encode(%{ reply: rev})
-    {:reply, {:text, reply}, req, state}
-  end
+    def create_connection(:permanent) do
+        ElixirPubsubConnection.Supervisor.start_connection(self(), :permanent, nil)
+        receive do ret -> ret end
+    end
 
-  def websocket_handle(_frame, _req, state) do
-    {:ok, state}
-  end
-  def websocket_info({_timeout, _ref, _msg}, req, state) do
-
-    time = time_as_string()
-    { :ok, message } = JSEX.encode(%{ time: time})
-    :erlang.start_timer(1000, self, [])
-    { :reply, {:text, message}, req, state}
-  end
-
-  def websocket_info(_info, _req, state) do
-    {:ok, state}
-  end
-
-  def time_as_string do
-    {hh, mm, ss} = :erlang.time()
-    :io_lib.format("~2.10.0B:~2.10.0B:~2.10.0B", [hh, mm, ss])
-    |> :erlang.list_to_binary()
-  end
-
+    def init_long_lived do
+        create_connection(:permanent)
+    end
 end
