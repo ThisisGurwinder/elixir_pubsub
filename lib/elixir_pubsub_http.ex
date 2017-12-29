0e50d5f7353813034f28143ab7de11744d0afa89
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-29 16:49:43 +0530

    wewerre

--- lib/elixir_pubsub_http.ex
+++ lib/elixir_pubsub_http.ex
@@ -3,6 +3,7 @@ defmodule ElixirPubsubHttp do
     def start(_type, _args) do
         dispatch_config = build_dispatch_config
         connection_sup_pid = spawn(fn -> ElixirPubsubConnection.Supervisor.start() end)
+        IO.puts "About to start http handler"
         { :ok, _ } = :cowboy.start_http(:http,
                         100,
                         [{:port, 8080}],
