1068c3bbd3ffaf3c617ca226a41cbdc3b751bf7f
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-29 23:33:35 +0530

    werwer

--- lib/elixir_pubsub_socket_handler.ex
+++ lib/elixir_pubsub_socket_handler.ex
@@ -37,4 +37,10 @@ defmodule ElixirPubsubSocketHandler do
     def init_long_lived do
         create_connection(:permanent)
     end
+
+    def time_as_string do
+        {hh, mm, ss} = :erlang.time()
+        :io_lib.format("~2.10.0B:~2.10.0B:~2.10.0B", [hh, mm, ss])
+        |> :erlang.list_to_binary()
+    end
 end
