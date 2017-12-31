0d96a271ccf03150cba35bef625b621a2fc34232
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-31 10:42:22 +0530

    lkjl

--- lib/elixir_pubsub_connection.ex
+++ lib/elixir_pubsub_connection.ex
@@ -29,7 +29,9 @@ defmodule ElixirPubsubConnection do
                     _ -> reset_timer(timer)
             end
         _state_new = case Poison.decode message do
-                        {:ok, parsed_message} -> process_message(parsed_message, state)
+                        {:ok, parsed_message} -> 
+                            IO.puts "Parsed Message #{inspect(parsed_message)}"
+                            process_message(parsed_message, state)
                         {:error, :badarg} -> send self(), {:just_send, "BAD ARGUMENT" }
                 end
         {:noreply, state}
