35e64fc31114d57340179e407dd3ee9882879c7c
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-31 13:08:19 +0530

    qweqwe

--- lib/elixir_pubsub_subscriber_sup.ex
+++ lib/elixir_pubsub_subscriber_sup.ex
@@ -1,20 +1,18 @@
 defmodule ElixirPubsubSubscriber.Supervisor do
-    use Supervisor
+  use Application
+  import Supervisor.Spec, warn: false
 
-    def start_link(_) do
-        {:ok, sup} = Supervisor.start_link(__MODULE__, [], name: :ElixirPubsubSubscriberSupervisor)
-    end
-    def start_child(args) do
-        Supervisor.start_child(__MODULE__, args)
-    end
-    def init(_) do
-        {:ok, {{:simple_one_for_one, 10, 10}, [
-            {:elixir_pubsub_subscriber,
-                {:elixir_pubsub_subscriber, :start_link, []},
-                :temporary,
-                :infinity,
-                :worker,
-                [:elixir_pubsub_subscriber]
-                } ]}}
-    end
+  def start() do
+
+    children = [
+      worker(ElixirPubsubSubscriber, [])
+    ]
+
+    opts = [strategy: :simple_one_for_one, name: ElixirPubsubSubscriber.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def start_child(args) do
+    Supervisor.start_child(ElixirPubsubSubscriber.Supervisor, args)
+  end
 end
