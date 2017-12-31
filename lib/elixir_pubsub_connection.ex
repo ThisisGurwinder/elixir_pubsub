8368f95ff4ba3849cab17b19bef34e7b45cec665
Gurwinder <ThisisGurwinder@Gmail.com>
2017-12-31 11:57:19 +0530

    lkjl

--- lib/elixir_pubsub_connection.ex
+++ lib/elixir_pubsub_connection.ex
@@ -63,7 +63,7 @@ defmodule ElixirPubsubConnection do
                 end
         Map.merge(state, %{:subscribers => new_subs})
     end
-    def process_message({:channel: channel, :publish: message}, state) do
+    def process_message({"channel" => channel, "publish" => message}, state) do
         IO.puts "Got the message #{inspect(channel)} and message #{inspect(message)}"
     end
     def process_message({%{"channel" => channel, "publish" => message}}, %{:publishers => publishers, :user_id => user_id, :user_data => user_data } = state) do
