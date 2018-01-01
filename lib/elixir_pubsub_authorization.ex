e604cc40516f6dd313d251740fd3401922f94117
Gurwinder <ThisisGurwinder@Gmail.com>
2018-01-01 20:56:34 +0530

    werwer

--- lib/elixir_pubsub_authorization.ex
+++ lib/elixir_pubsub_authorization.ex
@@ -21,12 +21,11 @@ defmodule ElixirPubsubAuthorization do
 			{:authorization_url, authorize_url} ->
 				case :httpc.request(:post, {authorize_url, [],  "application/x-www-form-urlencoded", "userid="++binary_to_list(user_id)++"&channel="++binary_to_list(channel)}, [], []) do
 					{:ok, {{_version, 200, _reason_phrase}, _headers, body}} ->
-						try Poison.decode(:binary.list_to_bin(body)) do
-							response -> case response do
-											[{"authorized", :true}] -> :true
-											[{"authorized", :false}] -> "no authorization"
-							_smth ->
-								"bad response from server on authorization"
+						try do
+							case Poison.decode(:binary.list_to_bin(body)) do
+								[{"authorized", :true}] -> :true
+								[{"authorized", :false}] -> "no authorization"
+								_smth -> "bad response from server on authorization"
 							end
 						catch 
 							_ -> "malformed json from server on authentication"
